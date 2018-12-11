ba17d985c7377504bb202979273ec6a043bc7f72
zollty <zollty@163.com>
2018-12-11 18:29:40 +0800

    polish comment

--- DropdownItem.cs
+++ DropdownItem.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿// Copyright (c) zollty (zolltytsou@gmail.com). All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
--- DropdownTextBox.cs
+++ DropdownTextBox.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿// Copyright (c) zollty (zolltytsou@gmail.com). All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Windows;
 using System.Windows.Controls;
 using System.Collections;
 
